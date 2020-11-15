54abdd5132f8ffc8a41fa4cce3cea555ac115766
Pepeb <mhmdfebian@gmail.com>
2020-11-15 23:02:17 +0700

    coomit

---
+++ database/migrations/2020_11_04_123813_create_karyawan_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKaryawanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->char('idkaryawan',6)->unique();
+            $table->string('divisi');
+            $table->string('ttl');
+            $table->string('jeniskelamin');
+            $table->string('email')->unique();
+            $table->string('nohp')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawan');
+    }
+}
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>amandah</title>
+  </head>
+  <body>
+  <!-- Just an image -->
+
+  <div class="container">
+  <h1>Safety Checking</h1>
+
+  <button type="button" class="btn btn-primary">Tambah Pekerja</button>
+
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Nama</th>
+            <th scope="col">ID</th>
+            <th scope="col">Jenis Kelamin</th>
+            <th scope="col">Divisi</th>
+            <th scope="col">Status</th>
+            <th scope="col">Edit</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($karyawan as $karyawan)
+            <tr>
+                <td>{{ $karyawan->nama }} {{ $karyawan->idkaryawan }}</td>
+                <td>{{ $karyawan->idkaryawan }}</td>
+                <td>{{ $karyawan->jeniskelamin }}</td>
+                <td>{{ $karyawan->divisi }}</td>
+                <td>Welding</td>
+                <td>Edit Delete</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
