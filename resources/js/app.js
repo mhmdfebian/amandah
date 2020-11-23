5b4448530dda61bd3c5af04fc69aa8b88c5e4e7e
Pepeb <mhmdfebian@gmail.com>
2020-11-23 18:55:01 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/mhmdfebian/amandah""
    This reverts commit 4f5838b3d92fdc7fe4175f50e6e4ab842f6fbdf8.

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,3 @@ $('.datepicker').datepicker();
 
 import 'jquery-ui/themes/base/all.css';
 import 'jquery-ui/ui/widgets/autocomplete.js';
-
-require('datatables.net-bs4');
-require('datatables.net-buttons-bs4');
